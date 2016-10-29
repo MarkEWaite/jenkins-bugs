2c68d9f85ee78203b53c748035e26126c22e15f8
Mark Waite <mark.earl.waite@gmail.com>
2016-10-28 21:55:27 -0600

    Add logContains global function

---
+++ vars/logContains.groovy
@@ -0,0 +1,7 @@
+def call(regex, failure_message) {
+  if (!manager.logContains(regex)) {
+    manager.addWarningBadge(failure_message)
+    manager.createSummary("warning.gif").appendText("<h1>" + failure_message + "</h1>", false, false, false, "red")
+    manager.buildUnstable()
+  }
+}
