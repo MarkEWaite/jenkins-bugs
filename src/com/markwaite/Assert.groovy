ba20f26b8d1cbab7f92069ad5395a7b9765b798e
Mark Waite <mark.earl.waite@gmail.com>
2016-10-28 21:28:53 -0600

    Add Assert to com.markwaite package, def the logContains method

--- src/com/markwaite/Assert.groovy
+++ src/com/markwaite/Assert.groovy
@@ -1,7 +1,7 @@
-#!/usr/bin/env groovy
+package com.markwaite;
 
 /* Assert build log contains string matching regex */
-void logContains(String regex, String failure_message) {
+def logContains(String regex, String failure_message) {
   if (!manager.logContains(regex)) {
     manager.addWarningBadge(failure_message)
     manager.createSummary("warning.gif").appendText("<h1>" + failure_message + "</h1>", false, false, false, "red")
