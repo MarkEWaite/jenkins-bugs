028bd80a6987e0cdc7d2ae4b5e7d61037d4483bb
Mark Waite <mark.earl.waite@gmail.com>
2016-10-28 21:59:53 -0600

    Refer to manager as argument

--- vars/logContains.groovy
+++ vars/logContains.groovy
@@ -1,4 +1,4 @@
-def call(regex, failure_message) {
+def call(manager, regex, failure_message) {
   if (!manager.logContains(regex)) {
     manager.addWarningBadge(failure_message)
     manager.createSummary("warning.gif").appendText("<h1>" + failure_message + "</h1>", false, false, false, "red")
