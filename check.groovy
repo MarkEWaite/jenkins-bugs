b0cc88138f60a66b77274cefaa2fa398c37c550d
Mark Waite <mark.earl.waite@gmail.com>
2016-10-28 22:49:19 -0600

    Add check script for assertions

---
+++ check.groovy
@@ -0,0 +1,27 @@
+/**
+ * Various assertions for Jenkinsfile use
+ */
+
+import com.cloudbees.groovy.cps.NonCPS
+
+def version = '1.0'
+
+@NonCPS
+def dumpEnvVars() {
+  def str = "Dumping build environment variables...\n"
+  for (Map.Entry<String, String> entry : currentBuild.build().environment) {
+    str += "    ${entry.key} = ${entry.value}\n"
+  }
+  echo str
+}
+
+/* Assert build log contains string matching regex */
+def logContains(String regex, String failure_message) {
+  if (!manager.logContains(regex)) {
+    manager.addWarningBadge(failure_message)
+    manager.createSummary("warning.gif").appendText("<h1>" + failure_message + "</h1>", false, false, false, "red")
+    manager.buildUnstable()
+  }
+}
+
+return this;
