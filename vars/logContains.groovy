8e082996970c66b091dd8480f3ec5cf315c0fcf4
Mark Waite <mark.earl.waite@gmail.com>
2016-10-29 06:54:22 -0600

    Remove pipeline global libraries - use separate repo

--- vars/logContains.groovy
+++
@@ -1,7 +0,0 @@
-def call(regex, failure_message) {
-  if (!manager.logContains(regex)) {
-    manager.addWarningBadge(failure_message)
-    manager.createSummary("warning.gif").appendText("<h1>" + failure_message + "</h1>", false, false, false, "red")
-    manager.buildUnstable()
-  }
-}
