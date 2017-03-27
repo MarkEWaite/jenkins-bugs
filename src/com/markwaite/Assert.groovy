6b88608c08b8df545cc9534dd152f63e55b142de
Mark Waite <mark.earl.waite@gmail.com>
2017-03-27 06:53:12 -0600

    Add an assertCondition method for general use
    Reduce code duplication

--- src/com/markwaite/Assert.groovy
+++ src/com/markwaite/Assert.groovy
@@ -1,19 +1,19 @@
 package com.markwaite;
 
-/* Assert build log contains string matching regex */
-def logContains(String regex, String failure_message) {
-  if (!manager.logContains(regex)) {
+def assertCondition(boolean condition, String failure_message) {
+  if (!condition)) {
     manager.addWarningBadge(failure_message)
     manager.createSummary("warning.gif").appendText("<h1>" + failure_message + "</h1>", false, false, false, "red")
     manager.buildUnstable()
   }
 }
 
+/* Assert build log contains string matching regex */
+def logContains(String regex, String failure_message) {
+  assertCondition(manager.logContains(regex), failure_message)
+}
+
 /* Assert build log does not contain string matching regex */
 def logDoesNotContain(String regex, String failure_message) {
-  if (manager.logContains(regex)) {
-    manager.addWarningBadge(failure_message)
-    manager.createSummary("warning.gif").appendText("<h1>" + failure_message + "</h1>", false, false, false, "red")
-    manager.buildUnstable()
-  }
+  assertCondition(!manager.logContains(regex), failure_message)
 }
