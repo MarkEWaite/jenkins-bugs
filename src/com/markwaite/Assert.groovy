99421cf0a23e9b75b301e5681df1f444d9d05258
Mark Waite <mark.earl.waite@gmail.com>
2017-05-28 10:23:06 -0600

    Add logContainsCount assertion

--- src/com/markwaite/Assert.groovy
+++ src/com/markwaite/Assert.groovy
@@ -13,6 +13,16 @@ def logContains(String regex, String failure_message) {
   assertCondition(manager.logContains(regex), failure_message)
 }
 
+/* Assert build log contains exactly count strings matching regex */
+def logContainsCount(int expectedCount, String regexp, String failure_message) {
+  java.util.regex.Matcher matcher = getLogMatcher(regexp)
+  int count = 0
+  while (matcher.find()) {
+    count++
+  }
+  assertCondition(count == expectedCount, "Found " + count + ", expected " + expectedCount. " + failure_message)
+}
+
 /* Assert build log does not contain string matching regex */
 def logDoesNotContain(String regex, String failure_message) {
   assertCondition(!manager.logContains(regex), failure_message)
