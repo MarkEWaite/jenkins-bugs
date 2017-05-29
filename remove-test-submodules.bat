742858c60141aadf41481610e37d1a77f147fc07
Mark Waite <mark.earl.waite@gmail.com>
2017-05-28 21:42:44 -0600

    Add remove-test-submodules batch file

---
+++ remove-test-submodules.bat
@@ -0,0 +1,14 @@
+REM Remove all submodules from test-submodules directory
+
+REM See https://stackoverflow.com/questions/1260748/how-do-i-remove-a-submodule
+
+FOR /D %%T IN ("test-submodules\*") do (
+
+    RMDIR /S/Q %%T
+    git submodule deinit -f -- %%T
+    RMDIR .git\modules\%%T
+
+    git rm -f %%T
+    git commit -m "Remove submodule %%~nxT"
+
+)
