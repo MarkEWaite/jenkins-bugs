d432f59249e5d2f08a8cec313804d6bdcfe85b8f
Mark Waite <mark.earl.waite@gmail.com>
2019-07-14 05:53:20 -0600

    Remove bare and cache as remotes during increment
    Original bug verification attempt intentionally relied on polling without
    the benefit of a notifyCommit.  Use the increment task to assure that
    those conditions are the same even when this repository is cloned from
    a typical make_branches call from the master branch.

---
+++ remove-remotes.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Intentionally did not define the cache repository so that no notifyCommit
+# will be called for commits to this branch.  GitHub webhooks are not
+# currently configured to reach inside my private network, so changes
+# on this branch should only be detected by the polling defined in the
+# Jenkinsfile.
+
+# The typical process to create this repository will add a cache and
+# a bare remote.  This script removes them so that the test environment
+# matches the original verification attempt.
+
+git remote | grep -q -w cache && git remote rm cache
+git remote | grep -q -w bare && git remote rm bare
+
+exit 0
